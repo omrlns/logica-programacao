835815a7c480d0bf0c176ad567b95f833e4ad418
Marlon <marlon.da.silva996@gmail.com>
2026-04-14 01:05:58 -0300

    trabalhando com "números"

--- Capítulo 02/script.js
+++ Capítulo 02/script.js
@@ -1,5 +1,22 @@
-alert("Olá mundo!");
-alert("Esse é o meu segundo programa!");
+// alert("Olá mundo!");
+// alert("Esse é o meu segundo programa!");
 
-document.write("Olá mundo!<br>") // isso escreve diretamente no index.html
-document.write("Esse é o meu segundo programa!")
+document.write("Olá mundo!<br>"); // isso escreve diretamente no index.html
+document.write("Esse é o meu segundo programa!");
+
+document.write("<br>A minha idade é: ");
+document.write(22, "<br>");
+
+document.write(25 + 25); // resultado = 50 (soma dos valores)
+document.write("25" + "25 <br>"); // resultado = 2525 (concatenação dos valores)
+
+document.write("Eu nasci em ");
+document.write(2026 - 23, "<br>");
+
+document.write("A soma das primeiras três dezenas é: ");
+document.write(10 + 20 + 30, "<br>");
+
+document.write("A média das primeiras três dezenas é: ");
+document.write((10 + 20 + 30) / 3, "<br>");
+
+document.write("A minha idade é " + 22)
