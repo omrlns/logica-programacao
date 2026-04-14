b1febb7880d317c6359537dafad94f2af0d99893
Marlon <marlon.da.silva996@gmail.com>
2026-04-14 00:47:22 -0300

    dê olá ao mundo de outras formas

---
+++ Capítulo 02/script.js
@@ -0,0 +1,5 @@
+alert("Olá mundo!");
+alert("Esse é o meu segundo programa!");
+
+document.write("Olá mundo!<br>") // isso escreve diretamente no index.html
+document.write("Esse é o meu segundo programa!")
