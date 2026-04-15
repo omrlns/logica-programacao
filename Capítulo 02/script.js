9e950d00d25876c604ec611a2b586ddc86893d7c
Marlon <marlon.da.silva996@gmail.com>
2026-04-15 18:17:19 -0300

    revisando o tópico sobre números

--- Capítulo 02/script.js
+++ Capítulo 02/script.js
@@ -19,4 +19,9 @@ document.write(10 + 20 + 30, "<br>");
 document.write("A média das primeiras três dezenas é: ");
 document.write((10 + 20 + 30) / 3, "<br>");
 
-document.write("A minha idade é " + 22)
+document.write("A minha idade é " + 22, "<br>");
+document.write("A soma das nossas idades é: " + (22 + 20 + 21), "<br>")
+document.write("A média das nossas idades é: " + ((22 + 20 + 21) / 3), "<br>");
+document.write("Eu tenho " + 22 + " anos e o meu irmão tem " + 20 + " anos", "<br>")
+document.write("A diferença de idade entre nós dois é de " + (22 -  20) + " anos", "<br>")
+document.write("Em média, um casal gera um filho aos 28 anos de idade, então podemos considerar que desde o 'Ano 0' se passaram " + ((2026 / 28) | 0) + " gerações", "<br>")
