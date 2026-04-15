930d03b327aee7244f33805209e432c288303b82
Marlon <marlon.da.silva996@gmail.com>
2026-04-15 20:29:18 -0300

    alterando o nome da função

--- Capítulo 02/script.js
+++ Capítulo 02/script.js
@@ -1,58 +1,58 @@
 // alert("Olá mundo!");
 // alert("Esse é o meu segundo programa!");
 
-function pulaLinha() {
+function pularLinha() {
     document.write("<hr>");
 }
 
 document.write("Olá mundo!"); // isso escreve diretamente no index.html
-pulaLinha()
+pularLinha()
 document.write("Esse é o meu segundo programa!");
 
 document.write("<br>A minha idade é: ");
 document.write(22);
-pulaLinha()
+pularLinha()
 
 document.write(25 + 25); // resultado = 50 (soma dos valores)
 document.write("25" + "25"); // resultado = 2525 (concatenação dos valores)
-pulaLinha()
+pularLinha()
 
 document.write("Eu nasci em ");
 document.write(2026 - 23);
-pulaLinha()
+pularLinha()
 
 document.write("A soma das primeiras três dezenas é: ");
 document.write(10 + 20 + 30);
-pulaLinha()
+pularLinha()
 
 document.write("A média das primeiras três dezenas é: ");
 document.write((10 + 20 + 30) / 3);
-pulaLinha()
+pularLinha()
 
 document.write("A minha idade é " + 22);
-pulaLinha()
+pularLinha()
 document.write("A soma das nossas idades é: " + (22 + 20 + 21));
-pulaLinha()
+pularLinha()
 document.write("A média das nossas idades é: " + ((22 + 20 + 21) / 3));
-pulaLinha()
+pularLinha()
 document.write("Eu tenho " + 22 + " anos e o meu irmão tem " + 20 + " anos");
-pulaLinha()
+pularLinha()
 document.write("A diferença de idade entre nós dois é de " + (22 -  20) + " anos");
-pulaLinha()
+pularLinha()
 document.write("Em média, um casal gera um filho aos 28 anos de idade, então podemos considerar que desde o 'Ano 0' se passaram " + (2026 / 28).toFixed(0) + " gerações.");
-pulaLinha()
+pularLinha()
 
 var ano = 2026;
 document.write("Eu nasci em " + (ano - 23));
-pulaLinha()
+pularLinha()
 document.write("O Gustavo nasceu em " + (ano - 21));
-pulaLinha()
+pularLinha()
 document.write("A Taíse nasceu em " + (ano - 20));
-pulaLinha()
+pularLinha()
 
 var marlonIdade = 22; gustavoIdade = 20; taiseIdade = 20;
 var totalIdades = marlonIdade + gustavoIdade + taiseIdade;
 document.write("A soma das nossas idades é: " + totalIdades);
-pulaLinha()
+pularLinha()
 var mediaIdades = (marlonIdade + gustavoIdade + taiseIdade) / 3;
 document.write("A média das nossas idades é: " + mediaIdades.toFixed(0));
