a01e406a8b652ad9d4de7e2c235f5b029ca323f4
Marlon <marlon.da.silva996@gmail.com>
2026-04-15 23:41:22 -0300

    alinhando as variáveis

--- Capítulo 02/script.js
+++ Capítulo 02/script.js
@@ -18,10 +18,7 @@ escrever(`O Marlon tem ${22} anos e o seu irmão Gustavo tem ${20} anos.`);
 escrever(`A diferença de idade entre os dois irmãos é de ${2} anos.`);
 escrever(`Em média, um casal gera um filho aos 28 anos de idade, então podemos considerar que desde o "Ano 0" se passaram ${(2026 / 28).toFixed(0)} gerações.`);
 
-var ano = 2026;
-var marlonIdade = 23; 
-var gustavoIdade = 21; 
-var taiseIdade = 20;
+var ano = 2026; var marlonIdade = 23; var gustavoIdade = 21; var taiseIdade = 20;
 var totalIdades = marlonIdade + gustavoIdade + taiseIdade;
 var mediaIdades = ((marlonIdade + gustavoIdade + taiseIdade) / 3).toFixed(0);
 
