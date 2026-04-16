806fec43df7532d7c350260c0b977cdad85c5ba1
Marlon <marlon.da.silva996@gmail.com>
2026-04-16 00:08:57 -0300

    ;

--- Capítulo 02/utilitarios.js
+++ Capítulo 02/utilitarios.js
@@ -2,5 +2,5 @@ export function escrever(texto, idElemento) {
     // seleciona o elemento que criamos no html
     const saida = document.getElementById(idElemento);
     // adiciona o texto dentro do elemento e quebra a linha
-    saida.innerHTML += texto + "<hr>"
+    saida.innerHTML += texto + "<hr>";
 }
