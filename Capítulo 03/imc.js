ebccabb1e845c5c2b438548868f542fb319f0073
Marlon <marlon.da.silva996@gmail.com>
2026-04-20 21:06:06 -0300

    capturando dados com o prompt()

--- Capítulo 03/imc.js
+++ Capítulo 03/imc.js
@@ -4,7 +4,7 @@ let peso = 78;
 let altura = 1.80;
 let imc = (peso / (altura * altura)).toFixed(0);
 
-escrever(`O IMC do usuário é: ${imc}.`, "resultado")
+escrever(`O IMC do usuário é: ${imc}.`, "resultado");
 
 function calcularIMC(peso, altura) {
     const imc = (peso / (altura * altura)).toFixed(0);
@@ -12,6 +12,11 @@ function calcularIMC(peso, altura) {
 }
 
 // calcularIMC(78, 1.80) // a ordem dos paramêtros influencia diretamente no resultado
-escrever(`Resultado do IMC: ${calcularIMC(78, 1.80)}.`, "resultado")
-escrever(`O usuário está "${calcularIMC(78, 1.80) - 18.5} pontos" acima do limite da magreza!`, "resultado")
-escrever(`O usuário está "${calcularIMC(78, 1.80) - 35} pontos" acima do limite da obesidade severa!`, "resultado")
+escrever(`Resultado do IMC: ${calcularIMC(78, 1.80)}.`, "resultado");
+escrever(`O usuário está "${calcularIMC(78, 1.80) - 18.5} pontos" acima do limite da magreza!`, "resultado");
+escrever(`O usuário está "${calcularIMC(78, 1.80) - 35} pontos" acima do limite da obesidade severa!`, "resultado");
+
+let nome = prompt("Bom dia! Qual é o seu nome?");
+let idade = prompt(`Prazer ${nome}, qual é a sua idade?`);
+
+escrever(`Pessoal, conheçam o(a) ${nome}, ele(a) tem ${idade} anos.`, "resultado");
