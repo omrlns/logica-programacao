3a8de3dd05870b995c44347d13d5f6bff8f1681a
Marlon <marlon.da.silva996@gmail.com>
2026-04-22 20:22:50 -0300

    setando o idElemento do método escrever()

--- Capítulo 02/utilitarios.js
+++ Capítulo 02/utilitarios.js
@@ -1,4 +1,4 @@
-export function escrever(texto, idElemento) {
+export function escrever(texto, idElemento="resultado") {
     // seleciona o elemento que criamos no html
     const saida = document.getElementById(idElemento);
     // adiciona o texto dentro do elemento e quebra a linha
