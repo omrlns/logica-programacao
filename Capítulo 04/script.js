65ba2154a40950ff01cbc7f5bb67f29d35955bef
Marlon <marlon.da.silva996@gmail.com>
2026-04-22 21:13:32 -0300

    refatorando o código com a aplicação do método calcularPontos() e capturando os dados do time adversário

--- Capítulo 04/script.js
+++ Capítulo 04/script.js
@@ -1,14 +1,23 @@
 import { escrever } from "../Capítulo 02/utilitarios.js"
 
-let nVitorias = parseInt(prompt("Quantos jogos o SPFC venceu?"));
-let nEmpates = parseInt(prompt("Quantos jogos o SPFC empatou?"));
-let pontos = (nVitorias * 3) + nEmpates;
+let nVitoriasA = parseInt(prompt("Quantos jogos o SPFC venceu?"));
+let nEmpatesA = parseInt(prompt("Quantos jogos o SPFC empatou?"));
+let nVitoriasB = parseInt(prompt("Quantos jogos o SCCP venceu?"));
+let nEmpatesB = parseInt(prompt("Quantos jogos o SCCP empatou?"));
 
-escrever(`O SPFC está com ${pontos} pontos.`);
+function calcularPontos(nVitorias, nEmpates) {
+    return (nVitorias * 3) + nEmpates;
+}
 
-if (pontos > 12) { // na data de hoje, 22/04/2026 o SCCP se encontra com 12 pontos na 17ª posição do campeonato
+let pontosTimeA = calcularPontos(nVitoriasA, nEmpatesA);
+let pontosTimeB = calcularPontos(nVitoriasB, nEmpatesB);
+
+escrever(`O SPFC está com ${pontosTimeA} pontos.`);
+escrever(`O SCCP está com ${pontosTimeB} pontos.`);
+
+if (pontosTimeA > pontosTimeB) {
     escrever(`O SPFC está com mais pontos e melhor posicionado no campeonato do que o SCCP!`);
-} else if (pontos == 12) {
+} else if (pontosTimeA == pontosTimeB) {
     escrever(`O SPFC e o SCCP estão empatados no campeonato!`);
 } else {
     escrever(`A campanha do SCCP é melhor do que a do SPFC.`);
